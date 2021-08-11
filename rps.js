eec89312a90eb05912e0d4adff355fe54f123124
lemur <noctiphobic@gmail.com>
2021-08-10 20:43:51 -0500

    made a js and an html file for rps project

---
+++ rps.js
@@ -0,0 +1,7 @@
+/*
+ * RULES:
+ * played from the console.
+ * function 1: play against computer: make a computerPlay fxn, that randomly returns r, p, or s. validate that it gives the expected output!
+ * function 2: plays a round of rps. has two parameters: playerSelection and computerSelection. return a string with either "you win!", "you lose! paper beats rock", or "it's a draw"
+ *
+ */
