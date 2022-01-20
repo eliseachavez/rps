a31d7b92c050b32e16a7f006e25aca4a4afbecd9
Elise <noctiphobic@gmail.com>
2022-01-19 19:17:24 -0600

    got the buttons with the event listeners working

--- rps.js
+++ rps.js
@@ -4,53 +4,53 @@ const scissorsbutton = document.querySelector('#scissors');
 
 
 rockbutton.addEventListener('click', () => {
-  let choice = playRound('rock', computerPlay());
-  alert(outcome);
+  let choice = playRound(computerPlay(),'rock');
+  alert(`${choice}`);
 });
 
 paperbutton.addEventListener('click', () => {
-  let choice = playRound('paper', computerPlay());
-  alert(outcome);
+  let choice = playRound(computerPlay(),'paper');
+  alert(`${choice}`);
 });
 
 scissorsbutton.addEventListener('click', () => {
-  let choice = playRound('scissors', computerPlay());
-  alert(outcome);
+  let choice = playRound(computerPlay(),'scissors');
+  alert(`${choice}`);
 });
 
 //***********FUNCTIONS************************************************************
 
 function computerPlay() {
-	let options = ['r', 'p', 's'];
+	let options = ['rock', 'paper', 'scissors'];
 
 	let i = Math.floor(Math.random() * 3 ); //get random number from 0 < 1. so .1, .2, .35, etc. then mult by max #. so .3 * 3 = .9. then floor that: 0. .9 * 3 = 2.7. floor is 2. can't go past the max num
 	if (typeof (options[i]) === 'string') {
-		alert(`Computer chose ${options[i]}`);
 		return options[i];
 	}
 }
 
 function playRound(c, p) {
+	alert(`computer chose ${c} and you chose ${p}`);
 	let outcome;
 	if (c === p)
 		outcome = 'tie';
-	else if (c === 'r' && p === 'p')
+	else if (c === 'rock' && p === 'paper')
 		outcome = "Player wins";
-	else if (c === 'r' && p === 's')
+	else if (c === 'rock' && p === 'scissors')
 		outcome = "Computer wins";
-	else if (c === 's' && p === 'p')
+	else if (c === 'scissors' && p === 'paper')
 		outcome = "Computer wins";
-	else if (p === 'r' && c === 'p')
+	else if (p === 'rock' && c === 'paper')
 		outcome = "Computer wins";
-	else if (p === 'r' && c === 's')
+	else if (p === 'rock' && c === 'scissors')
 		outcome = "Player wins";
-	else if (p === 's' && c === 'p')
+	else if (p === 'scissors' && c === 'paper')
 		outcome = "Player wins";
 	//just realized this is better as a switch statement
-	alert(`The outcome of this match is: ${outcome}`);
 	return outcome;
 }
 
+/*
 function playerSelection() {
 	let playerChoice = prompt("Enter your option: rock, paper, or scissors: \n");
 	let choice = playerChoice.toLowerCase();
@@ -64,12 +64,13 @@ function playerSelection() {
 		alert('What you typed was an invalid option. Perhaps it was mispelled? Please refresh the page to re-enter your choice.');
 	}
 }
+*/
 
-let playerChoice = playerSelection();
-let computerChoice = computerPlay();
+//let playerChoice = playerSelection();
+//let computerChoice = computerPlay();
+//let outcome = playRound(computerChoice, playerChoice);
 
-let outcome = playRound(computerChoice, playerChoice);
-
+/*
 function game() {
 	alert('Commencing a match of 5 games against computer! Brace yourself!');
 	let cScore = 0;
@@ -88,6 +89,6 @@ function game() {
 		alert(`Player won. Score was ${pScore}. Computer score was ${cScore}`);
 	return;
 }
-
+*/
 //*******MAIN****************************************************************************
-game();
+//game();
